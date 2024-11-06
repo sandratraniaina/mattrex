ee6bfcb6c2a8eb78cf90c4bc439d9b4d9ce47f0e
hasinajy <jyhasina@gmail.com>
2024-11-06 22:12:45 +0300

    feat: Add Block model

--- src/main/java/mg/sandratra/mattrex/model/Block.java
+++ src/main/java/mg/sandratra/mattrex/model/Block.java
@@ -3,6 +3,14 @@ package mg.sandratra.mattrex.model;
 public class Block {
     private Dimension dimension;
 
+    // Constructors
+    public Block() {
+    }
+
+    public Block(Dimension dimension) {
+        this.setDimension(dimension);
+    }
+
     // Getters and setters
     public Dimension getDimension() {
         return dimension;
@@ -10,5 +18,9 @@ public class Block {
 
     public void setDimension(Dimension dimension) {
         this.dimension = dimension;
+    }
+
+    public void setDimension(double width, double height, double depth) {
+        this.setDimension(new Dimension(width, height, depth));
     }
 }
