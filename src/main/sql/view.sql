9448e65f26238388fa394b330a57c734c48387df
hasinajy <jyhasina@gmail.com>
2024-11-07 10:14:40 +0300

    style: Format sql files

--- src/main/sql/view.sql
+++ src/main/sql/view.sql
@@ -1,4 +1,5 @@
-CREATE VIEW foam_with_dimensions AS
+CREATE VIEW
+    foam_with_dimensions AS
 SELECT
     f.*,
     d.width,
@@ -6,10 +7,10 @@ SELECT
     d.depth
 FROM
     foam f
-        JOIN
-    dimension d ON f.dimension_id = d.id;
+    JOIN dimension d ON f.dimension_id = d.id;
 
-CREATE VIEW size_type_with_dimensions AS
+CREATE VIEW
+    size_type_with_dimensions AS
 SELECT
     st.*,
     d.width,
@@ -17,5 +18,4 @@ SELECT
     d.depth
 FROM
     size_type st
-        JOIN
-    dimension d ON st.block_id = d.id;
+    JOIN dimension d ON st.block_id = d.id;
