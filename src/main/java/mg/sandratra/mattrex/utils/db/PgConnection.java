d5f0f67b74c9d13d46bbe325e9bde0995dfb84f4
hasinajy <jyhasina@gmail.com>
2024-11-07 10:23:32 +0300

    refactor: Use properties file in the PgConnection

--- src/main/java/mg/sandratra/mattrex/utils/db/PgConnection.java
+++ src/main/java/mg/sandratra/mattrex/utils/db/PgConnection.java
@@ -3,16 +3,44 @@ package mg.sandratra.mattrex.utils.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class PgConnection {
-    private static final String database = "mattrex";
-    private static final String user = "postgres";
-    private static final String password = "aina";
-    private static final String url = "jdbc:postgresql://localhost:5432/" + database;
+    private static String database;
+    private static String user;
+    private static String password;
+    private static String url;
+
+    static {
+        try (InputStream input = PgConnection.class.getClassLoader().getResourceAsStream("database.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find database.properties");
+                return;
+            }
+
+            // Load properties file
+            Properties properties = new Properties();
+            properties.load(input);
+
+            // Get properties from the file
+            database = properties.getProperty("database");
+            user = properties.getProperty("user");
+            password = properties.getProperty("password");
+            url = properties.getProperty("url") + database; // Append the database to the URL
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     // Methods
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        // Register the PostgreSQL driver
         Class.forName("org.postgresql.Driver");
+
+        // Establish the connection using properties loaded from the file
         return DriverManager.getConnection(url, user, password);
     }
 }
